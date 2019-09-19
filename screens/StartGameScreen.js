9ebc5ffe49f312a95ca8bf1f573e93c1ab1010dc
Vedant <mainvedant.vns123@gmail.com>
2019-09-19 23:00:41 +0530

    themefy

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View, Text, StyleSheet, TextInput, Button} from 'react-native';
 import  Card from '../components/Card';
+import Colors from '../constants/colors';
 
 const StartGameScreen = props =>{
 return (
@@ -10,8 +11,8 @@ return (
             <Text>Select a Number</Text>
             <TextInput />
             <View style={styles.buttonContainer}>
-                <Button title="Reset" onPress={() =>{}}/>
-                <Button title="Confirm" onPress={() =>{}}/>
+                <View style={styles.button}><Button title="Reset" color={Colors.accent} onPress={() =>{}}/></View>
+                <View style={styles.button}><Button title="Confirm" color={Colors.primary} onPress={() =>{}}/></View>
             </View>
         </Card>
     </View>
@@ -38,6 +39,9 @@ inputContainer:{
     width:300,
     maxWidth:'80%',
     alignItems:'center',
+},
+button:{
+     width:100
 }
 });
 
