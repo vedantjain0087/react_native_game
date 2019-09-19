2f44e1ec7928cd596daabd9e05e36bc2e7224086
Vedant <mainvedant.vns123@gmail.com>
2019-09-19 21:42:31 +0530

    shadow and layout

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -24,19 +24,30 @@ screen:{
     alignItems:"center"
 },
 title:{
-fontSize:20,
-marginVertical:10
+    fontSize:20,
+    marginVertical:10
 },
 buttonContainer:{
-flexDirection:'row',
-width:'100%',
-justifyContent:'space-between',
-paddingHorizontal:15
+    flexDirection:'row',
+    width:'100%',
+    justifyContent:'space-between',
+    paddingHorizontal:15
 },
 inputContainer:{
-width:300,
-maxWidth:'80%',
-alignItems:'center'
+    width:300,
+    maxWidth:'80%',
+    alignItems:'center',
+    shadowColor:'black',
+    shadowOffset:{
+        width:0,
+        height:2
+    },
+    shadowRadius:6,
+    shadowOpacity:0.26,
+    elevation:8,
+    backgroundColor:'white',
+    padding:20,
+    borderRadius:10
 }
 });
 
