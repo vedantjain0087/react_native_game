6d8af8292e233ef9690ddd823ccf0b7208e04b40
Vedant <mainvedant.vns123@gmail.com>
2019-09-26 18:38:05 +0530

    new game

--- App.js
+++ App.js
@@ -7,6 +7,10 @@ import GameOverScreen from './screens/GameOverScreen'
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
+  const configureNewGameHandler = () =>{
+    setGuessRounds(0);
+    setUserNumber(null);
+  }
   const startgameHandler = (selectedNumber) =>{
     setUserNumber(selectedNumber);
     setGuessRounds(0);
@@ -20,7 +24,7 @@ export default function App() {
     content = <GameScreen userChoice={userNumber} onGameOver = {gameOverHandler}/>
   }
   else if(guessRounds > 0){
-    content = <GameOverScreen />
+    content = <GameOverScreen roundsNumber={guessRounds} userNumber={userNumber} onRestart={configureNewGameHandler}/>
   }
   return (
     <View style={styles.screen}>
--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import  {View, Text, StyleSheet} from 'react-native';
+import  {View, Text, StyleSheet, Button} from 'react-native';
 
 const GameOverScreen = props => {
     return (
         <View style={styles.screen}>
             <Text>Game Over</Text>
+            <Text>Number Of rounds {props.roundsNumber}</Text>
+            <Text>The Number was : {props.userNumber}</Text>
+            <Button title="New Game" onPress={props.onRestart}/>
         </View>
     )
 }
